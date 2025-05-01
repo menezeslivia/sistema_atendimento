bd87c4cd7fb352d92827bc1be0a31ae97148f5e2
Lívia Menezes <169061107+menezeslivia@users.noreply.github.com>
2025-05-01 20:53:20 -0300

    Update random.utils.ts

--- sistema-de-atendimento/src/app/utils/random.utils.ts
+++ sistema-de-atendimento/src/app/utils/random.utils.ts
@@ -1,22 +1,18 @@
 import { TipoSenha } from '../models/enums';
 
-// Retorna tempo em segundos
 export function randomizeTempoAtendimento(tipo: TipoSenha): number {
   const minutos = {
-    SP: 15, // Prioritária
-    SG: 5,  // Geral
-    SE: 1,  // Exames
+    SP: 15,
+    SG: 5,
+    SE: 1,
   };
 
   switch (tipo) {
     case TipoSenha.PRIORITARIA:
-      // +/- 5 minutos aleatórios
       return (minutos.SP + randomBetween(-5, 5)) * 60;
     case TipoSenha.GERAL:
-      // +/- 3 minutos aleatórios
       return (minutos.SG + randomBetween(-3, 3)) * 60;
     case TipoSenha.EXAME:
-      // 95% dos casos: 1 minuto, 5%: 5 minutos
       return Math.random() < 0.95 ? minutos.SE * 60 : 5 * 60;
     default:
       return 0;
