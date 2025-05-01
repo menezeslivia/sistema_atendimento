deec8d1cbc3194a904fcddd8393cbf9123736a49
Lívia Menezes <169061107+menezeslivia@users.noreply.github.com>
2025-05-01 20:55:16 -0300

    Update ticket.model.ts

--- sistema-de-atendimento/src/app/models/ticket.model.ts
+++ sistema-de-atendimento/src/app/models/ticket.model.ts
@@ -3,10 +3,10 @@ import {StatusTicket } from './enums';
 export type TipoSenha = 'SP' | 'SG' | 'SE';
 
 export interface Ticket {
-  codigo: string;          // Ex: 250428-SP001
+  codigo: string;         
   tipo: TipoSenha;
   status: StatusTicket;
   dataEmissao: Date;
-  dataAtendimento?: Date;  // Pode ser undefined se não foi atendido ainda
-  guicheAtendimento?: number; // Guichê que atendeu
+  dataAtendimento?: Date; 
+  guicheAtendimento?: number;
 }
