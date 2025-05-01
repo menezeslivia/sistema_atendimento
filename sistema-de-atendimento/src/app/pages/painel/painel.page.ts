334a0dc25f9c41ac7ef1c45ec3480ecb6105aee4
Lívia Menezes <169061107+menezeslivia@users.noreply.github.com>
2025-05-01 20:50:28 -0300

    Update painel.page.ts

--- sistema-de-atendimento/src/app/pages/painel/painel.page.ts
+++ sistema-de-atendimento/src/app/pages/painel/painel.page.ts
@@ -1,4 +1,3 @@
-// src/app/pages/painel/painel.page.ts
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
